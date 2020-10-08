82a9d4669ffd6d867e58d30ad198b8d4a3586549
Jorge Massih <jorgmassih@gmail.com>
2020-10-08 17:29:51 -0400

    - Virtual Ip show option in DeviceList.js component.

--- src/Components/DeviceList/DeviceList.js
+++ src/Components/DeviceList/DeviceList.js
@@ -8,13 +8,13 @@ export default class DeviceList extends Component {
         super(props);
         this.state = {
             list: [
-                {ip:'10.10.10.1', mac: 'ff:ff:ff:ff:00'},
-                {ip:'10.10.10.2', mac: 'ff:ff:ff:ff:01'},
-                {ip:'10.10.10.3', mac: 'ff:ff:ff:ff:02'},
-                {ip:'10.10.10.4', mac: 'ff:ff:ff:ff:03'},
-                {ip:'10.10.10.5', mac: 'ff:ff:ff:ff:04'},
-                {ip:'10.10.10.6', mac: 'ff:ff:ff:ff:05'},
-                {ip:'10.10.10.7', mac: 'ff:ff:ff:ff:06'},
+                {ip:'10.10.0.1', mac: 'ff:ff:ff:ff:00', virtualIp: '10.10.1.1'},
+                {ip:'10.10.0.2', mac: 'ff:ff:ff:ff:01', virtualIp: '10.10.1.2'},
+                {ip:'10.10.0.3', mac: 'ff:ff:ff:ff:02', virtualIp: '10.10.1.3'},
+                {ip:'10.10.0.4', mac: 'ff:ff:ff:ff:03', virtualIp: '10.10.1.4'},
+                {ip:'10.10.0.5', mac: 'ff:ff:ff:ff:04', virtualIp: '10.10.1.5'},
+                {ip:'10.10.0.6', mac: 'ff:ff:ff:ff:05', virtualIp: '10.10.1.6'},
+                {ip:'10.10.0.7', mac: 'ff:ff:ff:ff:06', virtualIp: ''},
             ]
         };
     }
@@ -64,7 +64,8 @@ export default class DeviceList extends Component {
                                         >
                                             <Popup.Content>
                                                 <p><b>MAC:</b> {item.mac.toUpperCase()}</p>
-                                                <p><b>IP:</b> {item.ip.toUpperCase()}</p>
+                                                <p><b>IP:</b> {item.ip}</p>
+                                                {(item.virtualIp !== '') && <p><b>VIRTUAL IP:</b> {item.virtualIp}</p>}
                                             </Popup.Content>
 
                                         </Popup>
